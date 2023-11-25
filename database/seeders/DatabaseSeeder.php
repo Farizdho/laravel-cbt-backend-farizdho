0f89d8231ca2d2d22e3a18109666a11746763932
Farizdho <Farizdho@gmail.com>
2023-11-25 18:48:08 +0700

    update soal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Soal;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(UsersSeeder::class);
+        $this->call([
+            UsersSeeder::class,
+            SoalSeeder::class,
+        ]);
     }
 }
